d69c552ee9ea2fe903f761809a5e93f4e31eca18
Michael Natterer <mitch@gimp.org>
2010-10-19 12:11:59 +0200

    app: use gtk_widget_get_preferred_size() instead of size_request()

--- app/display/gimpimagewindow.c
+++ app/display/gimpimagewindow.c
@@ -729,7 +729,7 @@ gimp_image_window_style_set (GtkWidget *widget,
 
   statusbar = gimp_display_shell_get_statusbar (shell);
 
-  gtk_widget_size_request (GTK_WIDGET (statusbar), &requisition);
+  gtk_widget_get_preferred_size (GTK_WIDGET (statusbar), &requisition, NULL);
 
   geometry.min_height = 23;
 
@@ -738,7 +738,7 @@ gimp_image_window_style_set (GtkWidget *widget,
 
   if (private->menubar)
     {
-      gtk_widget_size_request (private->menubar, &requisition);
+      gtk_widget_get_preferred_size (private->menubar, &requisition, NULL);
 
       geometry.min_height += requisition.height;
     }
