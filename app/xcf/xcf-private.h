2ae0b131c9f75a4217915e943ea9c4454256d738
Jehan <jehan@girinstud.io>
2013-04-29 10:53:36 +0900

    Bug 648776 - mirror symmetries.
    You can now set any paint tool to mirror any stroke relatively
    horizontal/vertical axis or a central point (any combination of these 3
    symmetries).
    This has been implemented as a new multi-stroke core, where every stroke
    is actually handled as a multi-stroke (of course by default, the multi-stroke
    is of size 1).
    Current version has to be activated in the playground.

--- app/xcf/xcf-private.h
+++ app/xcf/xcf-private.h
@@ -57,7 +57,8 @@ typedef enum
   PROP_ITEM_PATH          = 30,
   PROP_GROUP_ITEM_FLAGS   = 31,
   PROP_LOCK_POSITION      = 32,
-  PROP_FLOAT_OPACITY      = 33
+  PROP_FLOAT_OPACITY      = 33,
+  PROP_MULTI_STROKE       = 34
 } PropType;
 
 typedef enum
