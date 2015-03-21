91615e1d2a870d965d00ad33445e044221a92e8e
Jehan <jehan@girinstud.io>
2015-03-22 00:43:01 +0100

    app: better API for gimpimage-symmetry.h.

--- app/core/gimpimage.c
+++ app/core/gimpimage.c
@@ -701,8 +701,6 @@ gimp_image_init (GimpImage *image)
 
   private->symmetries          = NULL;
   private->selected_symmetry   = NULL;
-  private->id_symmetry         = gimp_image_symmetry_new (image,
-                                                          GIMP_TYPE_SYMMETRY);
 
   private->guides              = NULL;
   private->grid                = NULL;
@@ -1062,12 +1060,6 @@ gimp_image_finalize (GObject *object)
     {
       g_list_free_full (private->symmetries, g_object_unref);
       private->symmetries = NULL;
-    }
-
-  if (private->id_symmetry)
-    {
-      g_object_unref (private->id_symmetry);
-      private->id_symmetry = NULL;
     }
 
   if (private->grid)
